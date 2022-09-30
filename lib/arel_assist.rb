5924ec3d0287fee2c14f9cab7b1a0945ec7c5857
Eric Berry <eric@berry.sh>
2022-09-30 14:50:12 -0600

    initial commit

---
+++ lib/arel_assist.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "arel_assist/version"
+
+module ArelAssist
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/arel_assist/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ArelAssist
+  VERSION = "0.1.0"
+end
